c5325fc0fe7ad361c29f6d1515686684ecd650e0
Akushi <steven.lesaffre@gmail.com>
2026-09-17 18:37:24 +0200

    Ajout carousel version tablette et mobil

--- JS/carousel.js
+++ JS/carousel.js
@@ -14,23 +14,61 @@ function updateVideoCarousel() {
   // Angle entre chaque vidéo
   const angleStep = 360 / totalVideos;
 
-  // Rayon du cercle selon le nombre de vidéos
+  // Rayon du cercle selon la taille de l'écran ET le nombre de vidéos
   let radius;
 
-  if (totalVideos <= 3) {
-    radius = 400;
-  } else if (totalVideos <= 6) {
-    radius = 450;
-  } else if (totalVideos <= 8) {
-    radius = 500;
-  } else if (totalVideos <= 10) {
-    radius = 550;
-  } else if (totalVideos <= 12) {
-    radius = 600;
-  } else if (totalVideos <= 14) {
-    radius = 650;
+  if (window.innerWidth <= 767) {
+    // Mobile
+    if (totalVideos <= 3) {
+      radius = 150;
+    } else if (totalVideos <= 6) {
+      radius = 200;
+    } else if (totalVideos <= 8) {
+      radius = 250;
+    } else if (totalVideos <= 10) {
+      radius = 300;
+    } else if (totalVideos <= 12) {
+      radius = 325;
+    } else if (totalVideos <= 14) {
+      radius = 350;
+    } else {
+      radius = 375;
+    }
+  } else if (window.innerWidth <= 1440) {
+    // Petit écran / petit laptop
+
+    if (totalVideos <= 3) {
+      radius = 250;
+    } else if (totalVideos <= 6) {
+      radius = 325;
+    } else if (totalVideos <= 8) {
+      radius = 375;
+    } else if (totalVideos <= 10) {
+      radius = 400;
+    } else if (totalVideos <= 12) {
+      radius = 450;
+    } else if (totalVideos <= 14) {
+      radius = 475;
+    } else {
+      radius = 500;
+    }
   } else {
-    radius = 900;
+    // Grand écran
+    if (totalVideos <= 3) {
+      radius = 400;
+    } else if (totalVideos <= 6) {
+      radius = 450;
+    } else if (totalVideos <= 8) {
+      radius = 500;
+    } else if (totalVideos <= 10) {
+      radius = 550;
+    } else if (totalVideos <= 12) {
+      radius = 600;
+    } else if (totalVideos <= 14) {
+      radius = 650;
+    } else {
+      radius = 900;
+    }
   }
 
   videoSlides.forEach((video, index) => {
